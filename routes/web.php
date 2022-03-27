6ee68f50f8d0c5d0fb6b3d8da8c28ac5374fdb8b
Nuris Akbar <nuris.akbar@gmail.com>
2022-03-27 22:50:53 +0700

    disable register

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('home');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false, // Registration Routes...
+    'reset' => false, // Password Reset Routes...
+    'verify' => false, // Email Verification Routes...
+  ]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('project',App\Http\Controllers\ProjectController::class);
